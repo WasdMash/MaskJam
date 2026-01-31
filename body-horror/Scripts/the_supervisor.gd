8fe1a8410508882b7df466c1c8884a6534f6bad8
nana <nana.amfobrobbey@gmail.com>
2026-01-31 19:53:00 +0000

    Starting work on AI

--- body-horror/Scripts/the_supervisor.gd
+++ body-horror/Scripts/the_supervisor.gd
@@ -1,4 +1,25 @@
-extends MeshInstance3D
+extends CharacterBody3D
+
+@onready var navAgent : NavigationAgent3D = $NavigationAgent3D
+
+#We want to be using the states here to control the movement of the character
+func move_ai():
+	print("We moving rn")
+	var random_pos := Vector3.ZERO
+	random_pos.x = randf_range(-50.0, 50.0)
+	random_pos.x = randf_range(-50.0, 50.0)
+	navAgent.set_target_position(random_pos)
+	
+func _process(_delta):
+	await get_tree().create_timer(10.0).timeout
+	move_ai()	
+	
+func _physics_process(delta: float) -> void:
+	var destination = navAgent.get_next_path_position()
+	var local_destiation = destination - global_position
+	var direction = local_destiation.normalized()
+	velocity = direction * 5.0
+	move_and_slide()
 
 #Triggering the end of a current task
 func _on_area_3d_body_entered(body: Node3D) -> void:
@@ -14,7 +35,7 @@ func interact():
 		#This is a 2D array of the following format:
 			#If true, this was the better option - heal their insanity
 			#False - you chose the wrong action - cut down their insanity
-    GameManager.request_choices.emit([["Yes", true], ["No", false]])
+	GameManager.request_choices.emit([["Yes", true], ["No", false]])
 """
 
 #Triggering the start of a new task
