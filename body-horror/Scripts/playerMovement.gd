6be2e7d50041cd907ed463e3bf96a63f914bf226
nana <nana.amfobrobbey@gmail.com>
2026-01-31 14:31:57 +0000

    Polished movement after too long

--- body-horror/Scripts/playerMovement.gd
+++ body-horror/Scripts/playerMovement.gd
@@ -4,14 +4,12 @@ const SPEED = 5.0
 const TURNING_CONSTANT = 0.1
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-		#Has to be in the order of left, right, up and down
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	#Lowkey don't fully know why my cameraController rotation is off by 90 degrees but i don't really care
+	
 	# 1. Get the camera's forward and right vectors, but ignore the Y (vertical) component
-	var cam_forward = -$cameraController/camera_target/Camera3D.global_transform.basis.z
-	var cam_right = $cameraController/camera_target/Camera3D.global_transform.basis.x
+	var cam_forward = -$cameraController.global_transform.basis.z
+	var cam_right = $cameraController.global_transform.basis.x
+	
 	# 2. Flatten them so we don't move up/down
 	cam_forward.y = 0
 	cam_right.y = 0
@@ -20,18 +18,19 @@ func _physics_process(delta: float) -> void:
 	
 	# 3. Calculate movement relative to those flattened vectors
 	var direction = (cam_forward * -input_dir.y + cam_right * input_dir.x).normalized()
-	if input_dir != Vector2(0,0):
-		$MeshInstance3D.rotation_degrees.y = $cameraController.rotation.y - 90 - rad_to_deg(input_dir.angle())
-	
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		
+		# 4. Smoothly rotate the mesh to face the movement direction
+		var target_rotation = atan2(direction.x, direction.z)
+		$MeshInstance3D.rotation.y = lerp_angle($MeshInstance3D.rotation.y, target_rotation, TURNING_CONSTANT)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 	
-	#Camera movement time - apparently less buggy when after physics
+	# Keep camera following
 	$cameraController.position = lerp($cameraController.position, position, delta * 5)
